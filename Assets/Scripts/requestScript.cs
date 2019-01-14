c9f57b99c1010b8815047a61348c4fb0ea67c719
Jdelorim <joshuadelorimier@gmail.com>
2019-01-14 15:16:51 -0500

    simple request

---
+++ Assets/Scripts/requestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class requestScript : MonoBehaviour {
+
+	private const string URL = "http://www.omdbapi.com/?t=";
+	private const string TOKEN = "&apikey=trilogy";
+	public InputField movieNameinput;
+	public string movieName;
+
+	public Text ResponseText;
+	
+
+
+	public void Request() {
+		
+		Debug.Log("url string:"+URL+movieNameinput.text+TOKEN);
+		WWW request = new WWW(URL+movieNameinput.text+TOKEN);
+		StartCoroutine(OnResponse(request));
+	}
+	private IEnumerator OnResponse(WWW req){
+		yield return req;
+		Debug.Log("REq:"+ req.text);
+		ResponseText.text = req.text;
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
